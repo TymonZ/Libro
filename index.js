0b6ee3f5171661160c5f186bf2bcf8ce3a33e0e7
Tymon Zaniewski <tymon.zaniewski.pl@gmail.com>
2020-10-26 16:33:14 +0100

    removed gdrive support from 'master'

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ const { createLibrary } = require("./js/lib/createLibrary");
 
 const { collectFromChannel } = require("./js/lib/collectFromChannel");
 
-/* GOOGLE DRIVE */
-const { googleDriveSetup } = require("./js/cloud-sync/googleDriveSetup");
-
-const { backup2GoogleDrive } = require("./js/cloud-sync/backup2GoogleDrive")
-
 /* DISCORD.JS */
 const { prefix, token } = require('./config.json');
 
@@ -25,82 +20,78 @@ const Discord = require(`discord.js`);
 
 const client = new Discord.Client();
 
-// Connect with gdrive, then do callback function
-googleDriveSetup((auth) =>{ //auth is used in 'backup files' function
+//All the Discord.js stuff down here
+client.once('ready', () => {
+	client.user.setActivity(':: help');
+	console.log("DISCORD BOT READY");
+})
 
-	//All the Discord.js stuff down here
-	client.once('ready', () => {
-		client.user.setActivity(':: help');
-		console.log("DISCORD BOT READY");
-	})
+client.login(token);
 
-	client.login(token);
+client.on('channelUpdate', (channel) => {
+	updateChannelList(channel);
+});
 
-	client.on('channelUpdate', (channel) => {
-		updateChannelList(channel);
-	});
+client.on('message', message => {
+	// HELP
+	if(message.content == `${prefix} help` || message.content == `${prefix} h`) {
+	message.channel.send(
+		'**HELP:**\n`:: help`/`:: h` - list of commands\n`:: server id` - basic server info\n`:: channel id` - basic channel info\n \n**DO AFTER INVITING BOT:**\n`:: server init` - creates server library\n`:: channel collect` - adds channel to list of channels that bot is getting images from\n \n**OTHER COMMANDS:**\n`:: backup files` - back up saved images to google drive\n`:: skip`/`:: s` - omitts downloading of attached image\n`:: random image chan` - sends random image from channel you requested\n`:: update chanlist` - manualy updates channel names in library');
+	}
+	// SERVER ID
+	else if(message.content == `${prefix} server id`) {
+		getServerID(message.guild, message.channel);
+	}
+	// CHANNEL ID	
+	else if(message.content == `${prefix} channel id`) {
+		getChannelID(message.guild, message.channel);
+	}
+	// INITIATE
+	else if(message.content == `${prefix} server init`) {
+		createLibrary(message.guild, message.channel, auth);
+	}
+	// COLLECT FROM CHANNEL
+	else if(message.content == `${prefix} channel collect`) {
+		collectFromChannel(message.guild, message.channel);
+	}
+	// UPDATE CHANLIST
+	else if(message.content == `${prefix} update chanlist`) {
+		updateChannelList(message.channel);
+		message.react('✅');
+	}
+	// BACKUP FILES TO GOOGLE DRIVE
+	else if(message.content == `${prefix} backup files`) {
+		// backup2GoogleDrive(auth, message.guild);
+		backup2GoogleDrive(auth, message).catch(console.error);
+	}
+	// RANDOM IMAGE FROM NAME
+	else if(message.content.startsWith(`${prefix} random image chan`)) {
+		const args = message.content.slice(prefix.length).split(' ');
+		if(args.length === 5) {
+			randomImageName(message.guild, message.channel, args[4]);
+		}
+		else {
+			message.channel.send('This channel name do not exist. Check the spelling, or try updating channel names manually (`:: update chanlist`)');
+		}
+	}
+	//MAKE "SKIP" A VALID COMMAND
+	else if(message.content == `${prefix} s`|| message.content == `${prefix} skip`) {
+		
+	}
+	// ELSE
+	else if(message.content.startsWith(`${prefix}`)) {
+		message.channel.send("That's not a real command dude. Check te spelling, or use `:: h`");
+	}
 
-	client.on('message', message => {
-		// HELP
-		if(message.content == `${prefix} help` || message.content == `${prefix} h`) {
-		message.channel.send(
-			'**HELP:**\n`:: help`/`:: h` - list of commands\n`:: server id` - basic server info\n`:: channel id` - basic channel info\n \n**DO AFTER INVITING BOT:**\n`:: server init` - creates server library\n`:: channel collect` - adds channel to list of channels that bot is getting images from\n \n**OTHER COMMANDS:**\n`:: backup files` - back up saved images to google drive\n`:: skip`/`:: s` - omitts downloading of attached image\n`:: random image chan` - sends random image from channel you requested\n`:: update chanlist` - manualy updates channel names in library');
-		}
-		// SERVER ID
-		else if(message.content == `${prefix} server id`) {
-			getServerID(message.guild, message.channel);
-		}
-		// CHANNEL ID	
-		else if(message.content == `${prefix} channel id`) {
-			getChannelID(message.guild, message.channel);
-		}
-		// INITIATE
-		else if(message.content == `${prefix} server init`) {
-			createLibrary(message.guild, message.channel, auth);
-		}
-		// COLLECT FROM CHANNEL
-		else if(message.content == `${prefix} channel collect`) {
-			collectFromChannel(message.guild, message.channel);
-		}
-		// UPDATE CHANLIST
-		else if(message.content == `${prefix} update chanlist`) {
-			updateChannelList(message.channel);
-			message.react('✅');
-		}
-		// BACKUP FILES TO GOOGLE DRIVE
-		else if(message.content == `${prefix} backup files`) {
-			// backup2GoogleDrive(auth, message.guild);
-			backup2GoogleDrive(auth, message).catch(console.error);
-		}
-		// RANDOM IMAGE FROM NAME
-		else if(message.content.startsWith(`${prefix} random image chan`)) {
-			const args = message.content.slice(prefix.length).split(' ');
-			if(args.length === 5) {
-				randomImageName(message.guild, message.channel, args[4]);
-			}
-			else {
-				message.channel.send('This channel name do not exist. Check the spelling, or try updating channel names manually (`:: update chanlist`)');
-			}
-		}
-		//MAKE "SKIP" A VALID COMMAND
-		else if(message.content == `${prefix} s`|| message.content == `${prefix} skip`) {
-			
-		}
-		// ELSE
-		else if(message.content.startsWith(`${prefix}`)) {
-			message.channel.send("That's not a real command dude. Check te spelling, or use `:: h`");
-		}
-
-		// DOWNLOAD
-		if(message.content == `${prefix} s`|| message.content == `${prefix} skip`) {
-			message.react('⏩');
-			console.log(`image omitted`);
-		}
-		else if(message.attachments.first()) {
-			imageSaver(
-				message,
-				message.attachments.first()
-			);
-		}
-	});
+	// DOWNLOAD
+	if(message.content == `${prefix} s`|| message.content == `${prefix} skip`) {
+		message.react('⏩');
+		console.log(`image omitted`);
+	}
+	else if(message.attachments.first()) {
+		imageSaver(
+			message,
+			message.attachments.first()
+		);
+	}
 });
